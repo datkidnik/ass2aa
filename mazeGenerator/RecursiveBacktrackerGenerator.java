6208268f6069db5dc024965ae385bba25f5baf85
Nik <s3379952@student.rmit.edu.au>
2017-10-17 02:59:20 +1100

    second commit, working on recur

--- mazeGenerator/RecursiveBacktrackerGenerator.java
+++ mazeGenerator/RecursiveBacktrackerGenerator.java
@@ -1,12 +1,23 @@
 package mazeGenerator;
 
+import java.util.*;
 import maze.Maze;
+import maze.Cell;
 
 public class RecursiveBacktrackerGenerator implements MazeGenerator {
+
+	//2D array to track which cells the algorithm has visited
+	protected boolean isVisited[][];
 
 	@Override
 	public void generateMaze(Maze maze) {
 		// TODO Auto-generated method stub
+
+		int sizeRow = maze.sizeR;
+		int sizeColumn = maze.sizeC;
+
+		isVisited = new boolean[sizeRow][sizeColumn];
+		Stack stackOfVisitedCells = new Stack();
 
 	} // end of generateMaze()
 
