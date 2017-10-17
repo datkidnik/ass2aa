5a1c7651abd6ddf69e4fd172808c99810a10270f
Nik <s3379952@student.rmit.edu.au>
2017-10-18 09:48:30 +1100

    have working recurMazeGen

--- mazeGenerator/MazeGenerator.java
+++ mazeGenerator/MazeGenerator.java
@@ -1,6 +1,8 @@
 package mazeGenerator;
 
+import java.util.*;
 import maze.Maze;
+import maze.Cell;
 
 /**
  * Interface of a maze generator.
@@ -16,4 +18,7 @@ public interface MazeGenerator
 	 * @param maze The reference of Maze object to generate. 
 	 */
 	public void generateMaze(Maze maze);
+
+
+	public void depthFirtstSearch(Maze maze, Cell cell, boolean arrayOfMaze[][], Queue<Cell> visitedCells);
 } // end of interface mazeGenerator
--- mazeGenerator/RecursiveBacktrackerGenerator.java
+++ mazeGenerator/RecursiveBacktrackerGenerator.java
@@ -1,24 +1,110 @@
 package mazeGenerator;
 
 import java.util.*;
+import java.util.Random;
 import maze.Maze;
 import maze.Cell;
 
 public class RecursiveBacktrackerGenerator implements MazeGenerator {
 
 	//2D array to track which cells the algorithm has visited
+	
 	protected boolean isVisited[][];
+	protected Queue<Cell> stackOfVisitedCells = new LinkedList<Cell>();
+	protected int totalCellCount;
 
 	@Override
 	public void generateMaze(Maze maze) {
 		// TODO Auto-generated method stub
-
+		
 		int sizeRow = maze.sizeR;
 		int sizeColumn = maze.sizeC;
 
-		isVisited = new boolean[sizeRow][sizeColumn];
-		Stack stackOfVisitedCells = new Stack();
+		totalCellCount = sizeRow*sizeColumn;
 
+		isVisited = new boolean[sizeRow][sizeColumn];
+		Cell entranceCell = maze.entrance;
+
+		depthFirtstSearch(maze, entranceCell, isVisited, stackOfVisitedCells);
 	} // end of generateMaze()
+
+	/*maze - the maze that we are generating 
+	*cell - the cell that we are currently at
+	*arrayOfMaze - array to check which cells have been visited and which have not
+	*visitedCells - a list of order of cells that have been visited
+	*
+	*/
+	public void depthFirtstSearch(Maze maze, Cell cell, boolean arrayOfMaze[][], Queue<Cell> visitedCells){
+		int currentRowCoord = cell.r;
+		int currentColumnCoord = cell.c;
+		Random rand = new Random();
+		int randomNumber;
+		Cell currentCell = cell;
+		boolean foundNextNeigh = false;
+
+		arrayOfMaze[currentRowCoord][currentColumnCoord] = true;
+		visitedCells.add(currentCell);
+		
+		for(int i = 0; i < 6; i++){
+			randomNumber = rand.nextInt(6);
+			if(i == 1 || 1 == 4){
+				continue;
+			}
+			else{
+				if( (currentCell.neigh[i] != null) && (arrayOfMaze[currentCell.neigh[i].r][currentCell.neigh[i].c] == false) ){
+					//REMEMBER THIS ------------------------
+					int neighborWallDirection = -1;
+
+					Cell nextCurrCell = currentCell.neigh[i];
+
+					/*switch(i){
+						case 0 :
+							neighborWallDirection = 3;
+						case 2 :
+							neighborWallDirection = 5;
+						case 3 :
+							neighborWallDirection = 0;
+						case 5 :
+							neighborWallDirection = 2;	
+						default :
+							System.out.println("error in RecursiveBacktrackerGenerator");
+					}*/
+
+
+					currentCell.wall[i].present = false;
+					//nextCurrCell.wall[neighborWallDirection].present = false;
+
+					maze.map[currentRowCoord][currentColumnCoord] = currentCell;
+
+					depthFirtstSearch(maze, nextCurrCell, arrayOfMaze, visitedCells);
+				}
+
+			}
+		}
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 } // end of class RecursiveBacktrackerGenerator
