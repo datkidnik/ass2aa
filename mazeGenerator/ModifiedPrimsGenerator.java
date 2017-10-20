5a4d6af3df95a1428d39f896dd7434c761fa99b7
Nik <s3379952@student.rmit.edu.au>
2017-10-20 23:23:12 +1100

    prim done prop

--- mazeGenerator/ModifiedPrimsGenerator.java
+++ mazeGenerator/ModifiedPrimsGenerator.java
@@ -8,7 +8,8 @@ import maze.Cell;
 public class ModifiedPrimsGenerator implements MazeGenerator {
 
 	protected boolean isVisited[][];
-	protected List<Cell> cellsWithNeighbors = new ArrayList<Cell>();
+	protected List<Cell> visitedCells = new ArrayList<Cell>();
+	protected List<Cell> cellArray = new ArrayList<Cell>();
 	protected int total;
 	protected int tally = 0;
 
@@ -26,18 +27,19 @@ public class ModifiedPrimsGenerator implements MazeGenerator {
 
 		isVisited[entranceCell.r][entranceCell.c] = true;
 
+		visitedCells.add(entranceCell);
 		//populates cellsWithNeighbors with the neighbors around the entrance cell
 		for(int i = 0; i < 6; i++){
 			if( (entranceCell.neigh[i] != null)){
-				cellsWithNeighbors.add(entranceCell.neigh[i]);		
+				cellArray.add( entranceCell.neigh[i] );		
 			}
 		}
-		primsAlgorithm(maze, isVisited, cellsWithNeighbors);
+		primsAlgorithm(maze, visitedCells, cellArray);
 	} // end of generateMaze()
 
 
-	public void primsAlgorithm(Maze maze, boolean arrayOfMaze[][], List<Cell> cellArray){		
-		
+	public void primsAlgorithm(Maze maze, List<Cell> visitedCells, List<Cell> cellArray){		
+
 		//list of directions of neighbors
 		List<Integer> directions = new ArrayList<Integer>();
 
@@ -55,138 +57,66 @@ public class ModifiedPrimsGenerator implements MazeGenerator {
 
 			//remove the next cell from the list of available neighbors
 			cellArray.remove(0);
+
+			visitedCells.add(currCell);
+
 			System.out.println(cellArray.size() + " 1");
 
 			//loops through neighbors of next cell and adds any neighbors to our list as long as its not already in the list or already visited
-			for(int i = 0; i < 6; i++){
-				if(i == 1 || i == 4){
-					continue;
-				}
-				else{
-					if( (currCell.neigh[i] != null) && (cellArray.contains(currCell.neigh[i]) == false) && (arrayOfMaze[currCell.neigh[i].r][currCell.neigh[i].c] == false)){
-						cellArray.add(currCell.neigh[i]);	
-						
-						System.out.println(cellArray.size() + " 2");
-
-					}
-				}
-			}
+			for(int i = 0; i < 6; i++){			
 	
-			while(found == false){
-				//initialising random generated number
-				int rand;
-				int counter = 4;
-				Random r = new Random();
-				rand = r.nextInt(counter);
-				
-				//adding potential directions for visisted cells to join the maze to
-				directions.add(0);
-				directions.add(2);
-				directions.add(3);
-				directions.add(5);
-				
-				//checks to see neighbor is part of the maze
-				if( (currCell.neigh[directions.get(rand)] != null) && (arrayOfMaze[currCell.neigh[directions.get(rand)].r][currCell.neigh[directions.get(rand)].c] == true) ){
+				if( (currCell.neigh[i] != null) && (cellArray.contains(currCell.neigh[i]) == false) && (visitedCells.contains( currCell.neigh[i] ) == false)){
+					cellArray.add(currCell.neigh[i]);	
+					System.out.println(cellArray.size() + " 2");
 
-					//removes the wall between current cell and the visited cell
-					currCell.wall[directions.get(rand)].present = false;
-
-					//marks this cell as visited	
-					arrayOfMaze[currCell.r][currCell.c] = true;
-
-					//updates the map
-					maze.map[currCell.r][currCell.c] = currCell;
-					System.out.println(cellArray.size() + " 3");
-
-					//change boolean value to end the loop
-					found = true;
-
-					//call function again
-					primsAlgorithm(maze, arrayOfMaze, cellArray);					
-				}
-				else{
-					counter--;
-					directions.remove(rand);
 				}	
-			}		
+			}
+			
+			//loops through neighbors of next cell and adds any neighbors to our list as long as its not already in the list or already visited
+			for(int i = 0; i < 6; i++){			
+	
+				if( (currCell.neigh[i] != null) && ( visitedCells.contains( currCell.neigh[i] ) == true)){
+					System.out.println("added dir");
+					directions.add(i);
+
+				}	
+			}
+
+			
+			//initialising random generated number
+			int rand;
+			int counter = directions.size();
+			Random r = new Random();
+			rand = r.nextInt(counter);
+			
+			
+			//checks to see neighbor is part of the maze
+			if( (currCell.neigh[directions.get(rand)] != null) && ( visitedCells.contains( currCell.neigh[directions.get(rand)] ) == true) ){
+
+				//removes the wall between current cell and the visited cell
+				currCell.wall[directions.get(rand)].present = false;
+
+
+				//updates the map
+				maze.map[currCell.r][currCell.c] = currCell;
+
+				System.out.println(cellArray.size() + " 3");
+
+				//change boolean value to end the loop
+				found = true;
+
+				//call function again
+				primsAlgorithm(maze, visitedCells, cellArray);					
+			}
+			else{
+				counter--;
+				directions.remove(rand);
+			}	
+				
 		}	
 	}
 
 
-	public void normal(Maze maze, boolean arrayOfMaze[][], List<Cell> cellArray){
-		//list of directions of neighbors
-		List<Integer> directions = new ArrayList<Integer>();
-
-		//initialising int
-		int random = 0;
-
-		//initialising found
-		boolean found = false;
-		
-
-		if(cellArray.size() > 0){
-
-			//the next cell is chosen from the top of the list of available neighbors
-			Cell currCell = cellArray.get(0);
-
-			//remove the next cell from the list of available neighbors
-			cellArray.remove(0);
-			System.out.println(cellArray.size() + " 1");
-
-			//loops through neighbors of next cell and adds any neighbors to our list as long as its not already in the list or already visited
-			for(int i = 0; i < 6; i++){
-				if(i == 1 || i == 4){
-					continue;
-				}
-				else{
-					if( (currCell.neigh[i] != null) && (cellArray.contains(currCell.neigh[i]) == false) && (arrayOfMaze[currCell.neigh[i].r][currCell.neigh[i].c] == false)){
-						cellArray.add(currCell.neigh[i]);	
-						
-						System.out.println(cellArray.size() + " 2");
-
-					}
-				}
-			}
-	
-			while(found == false){
-				//initialising random generated number
-				int rand;
-				int counter = 4;
-				Random r = new Random();
-				rand = r.nextInt(counter);
-				
-				//adding potential directions for visisted cells to join the maze to
-				directions.add(0);
-				directions.add(2);
-				directions.add(3);
-				directions.add(5);
-				
-				//checks to see neighbor is part of the maze
-				if( (currCell.neigh[directions.get(rand)] != null) && (arrayOfMaze[currCell.neigh[directions.get(rand)].r][currCell.neigh[directions.get(rand)].c] == true) ){
-
-					//removes the wall between current cell and the visited cell
-					currCell.wall[directions.get(rand)].present = false;
-
-					//marks this cell as visited	
-					arrayOfMaze[currCell.r][currCell.c] = true;
-
-					//updates the map
-					maze.map[currCell.r][currCell.c] = currCell;
-					System.out.println(cellArray.size() + " 3");
-
-					//change boolean value to end the loop
-					found = true;
-
-					//call function again
-					primsAlgorithm(maze, arrayOfMaze, cellArray);					
-				}
-				else{
-					counter--;
-					directions.remove(rand);
-				}	
-			}		
-		}	
-	}
 
 
 
--- mazeGenerator/RecursiveBacktrackerGenerator.java
+++ mazeGenerator/RecursiveBacktrackerGenerator.java
@@ -10,7 +10,8 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator {
 	//2D array to track which cells the algorithm has visited
 	
 	protected boolean isVisited[][];
-	protected List<Cell> stackOfVisitedCells = new ArrayList<Cell>();
+	protected List<Cell> visitedCells = new ArrayList<Cell>();
+	protected List<Cell> orderOfCells = new ArrayList<Cell>();
 	protected int totalCellCount;
 	protected int holder;
 
@@ -24,25 +25,14 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator {
 		totalCellCount = sizeRow*sizeColumn;
 
 
-		if(maze.type == 1){
-			holder = 11;
-		}
-		if(maze.type == 2){
-			holder = 12;
-		}
-		if(maze.type == 0){
-			holder = 10;
-		}
-
-		isVisited = new boolean[sizeRow][sizeColumn];
 		Cell entranceCell = maze.entrance;
 
-		depthFirtstSearch(maze, entranceCell, isVisited, stackOfVisitedCells);
+		depthFirtstSearch(maze, entranceCell, orderOfCells, visitedCells);
 	} // end of generateMaze()
 
 	/*maze - the maze that we are generating 
 	*cell - the cell that we are currently at
-	*arrayOfMaze - array to check which cells have been visited and which have not
+	*
 	*visitedCells - a list of order of cells that have been visited
 	*
 	*
@@ -57,22 +47,13 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator {
 	*
 	*
 	*/
-	public void depthFirtstSearch(Maze maze, Cell cell, boolean arrayOfMaze[][], List<Cell> visitedCells){
-		if(holder == 11){
-			System.out.println("tunnel");
-		}
-		if(holder == 12){
-			//holder = 0;
-			System.out.println("1");
-			hex(maze, cell, arrayOfMaze, visitedCells);
-		}
-		if(holder == 10){
-			//holder = 0;
-			normal(maze, cell, arrayOfMaze, visitedCells);
-		}
+	public void depthFirtstSearch(Maze maze, Cell cell, List<Cell> orderOfCells, List<Cell> visitedCells){
+		
+		normal(maze, cell, orderOfCells, visitedCells);
+		
 	}
 
-	public void normal(Maze maze, Cell cell, boolean arrayOfMaze[][], List<Cell> visitedCells){
+	public void normal(Maze maze, Cell cell, List<Cell> orderOfCells, List<Cell> visitedCells){
 		//list of directions in which neighbors exist
 		List<Integer> neighbors = new ArrayList<Integer>();
 
@@ -90,88 +71,20 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator {
 		//boolean used to track whether or not the current cell has univisited neighbors
 		boolean doesNeighExist = false;
 
-		//marking this cell as visited in our array to keep track of progress
-		arrayOfMaze[currentRowCoord][currentColumnCoord] = true;
+		if(visitedCells.contains(currentCell) == false){
+			//adding current cell to list of visited cells
+			visitedCells.add(currentCell);
+		}
 		
-		
+
 		//CHECK IF IT HAS UNVISITED NEIGHBORS
 		for(int i = 0; i < 6; i++){
 			
-				if( (currentCell.neigh[i] != null) && (arrayOfMaze[currentCell.neigh[i].r][currentCell.neigh[i].c] == false) ){
-					neighbors.add(i);
-					doesNeighExist = true;
-				}
-			
-		}
-		//CHOOSE NEIGHBOR IF ANY
-		if(doesNeighExist == true){
-				//random number generated to choose a random unvisited neighbor of current cell
-				randomNumber = rand.nextInt(neighbors.size());
-
-				//initialising the neighbor of the current cell that we've chosen at random 
-				Cell nextCurrCell = currentCell.neigh[neighbors.get(randomNumber)];
-
-				//removing the wall between current cell and next cell
-				currentCell.wall[neighbors.get(randomNumber)].present = false;
-
-				//updating the map to show new cell
-				maze.map[currentRowCoord][currentColumnCoord] = currentCell;
-
-				//adding current cell to list of visited cells
-				visitedCells.add(currentCell);
-
-				int shhhh = visitedCells.size();
-				System.out.println(shhhh + " does");
-
-				//pass in next cell into function to repeat process
-				normal(maze, nextCurrCell, arrayOfMaze, visitedCells);
-		}
-		else{
-			//gets the index of the last cell in the visitedCells list
-			int lastCellInArray = (visitedCells.size() - 1);
-
-			//if there is still cells left with potential unvisited neighbors, we do this
-			if(lastCellInArray >= 0){
-				Cell lastVisitedCell = visitedCells.get(lastCellInArray);
-				visitedCells.remove(lastCellInArray);
-
-				int shhhh2 = visitedCells.size();
-				System.out.println(shhhh2 + " does not");
-
-				normal(maze, lastVisitedCell, arrayOfMaze, visitedCells);
-			}
-		}
-	}
-
-
-	public void hex(Maze maze, Cell cell, boolean arrayOfMaze[][], List<Cell> visitedCells){
-		//list of directions in which neighbors exist
-		List<Integer> neighbors = new ArrayList<Integer>();
-
-		//coordinates of current cell
-		int currentRowCoord = cell.r;
-		int currentColumnCoord = cell.c;
-
-		//initialise random generator
-		Random rand = new Random();
-		int randomNumber;
-
-		//the current cell that we are stationed at
-		Cell currentCell = cell;
-
-		//boolean used to track whether or not the current cell has univisited neighbors
-		boolean doesNeighExist = false;
-
-		//marking this cell as visited in our array to keep track of progress
-		arrayOfMaze[currentRowCoord][currentColumnCoord] = true;
-		
-		
-		//CHECK IF IT HAS UNVISITED NEIGHBORS
-		for(int i = 0; i < 6; i++){
-			if( (currentCell.neigh[i] != null) && (arrayOfMaze[currentCell.neigh[i].r][currentCell.neigh[i].c] == false) ){
+			if( (currentCell.neigh[i] != null) && (visitedCells.contains(currentCell.neigh[i]) == false) ){
 				neighbors.add(i);
 				doesNeighExist = true;
 			}
+			
 		}
 		//CHOOSE NEIGHBOR IF ANY
 		if(doesNeighExist == true){
@@ -187,29 +100,36 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator {
 				//updating the map to show new cell
 				maze.map[currentRowCoord][currentColumnCoord] = currentCell;
 
-				//adding current cell to list of visited cells
-				visitedCells.add(currentCell);
+				
+				
+
+				orderOfCells.add(currentCell);
 
 				int shhhh = visitedCells.size();
 				System.out.println(shhhh + " does");
 
 				//pass in next cell into function to repeat process
-				hex(maze, nextCurrCell, arrayOfMaze, visitedCells);
+				normal(maze, nextCurrCell, orderOfCells, visitedCells);
 		}
 		else{
 			//gets the index of the last cell in the visitedCells list
-			int lastCellInArray = (visitedCells.size() - 1);
+			int lastCellInArray = (orderOfCells.size() - 1);
 
 			//if there is still cells left with potential unvisited neighbors, we do this
 			if(lastCellInArray >= 0){
-				Cell lastVisitedCell = visitedCells.get(lastCellInArray);
-				visitedCells.remove(lastCellInArray);
+				Cell lastVisitedCell = orderOfCells.get(lastCellInArray);
+				orderOfCells.remove(lastCellInArray);
 
 				int shhhh2 = visitedCells.size();
 				System.out.println(shhhh2 + " does not");
 
-				hex(maze, lastVisitedCell, arrayOfMaze, visitedCells);
+				normal(maze, lastVisitedCell, orderOfCells, visitedCells);
 			}
 		}
 	}
+
+
+	
+
+
 } // end of class RecursiveBacktrackerGenerator
